caa390ebaeaae46e986cb44e6591bfcebe74dc1c
sabgilhun <sabgilhun@gmail.com>
2022-08-15 23:38:46 +0900

    print 코드 추가

--- src/main/kotlin/parser/Parser.kt
+++ src/main/kotlin/parser/Parser.kt
@@ -2,6 +2,7 @@ package parser
 
 import Characters
 import ErrorHelper.throwError
+import Node
 import ReservedWords
 import scanner.Scanner
 import scanner.Token
@@ -13,7 +14,7 @@ class Parser(
     private var currentToken: Token? = scanner.scan()
 
     fun parse() {
-        parseProgram()
+        parseProgram(Node(0, "Program"))
         if (currentToken?.kind != Token.Kind.EOT) {
             throwError()
         }
@@ -31,55 +32,55 @@ class Parser(
         currentToken = scanner.scan()
     }
 
-    private fun parseProgram() {
-        parseSingleCommand()
+    private fun parseProgram(node: Node) {
+        parseSingleCommand(node.child("single-Command"))
     }
 
-    private fun parseCommand() {
-        parseSingleCommand()
+    private fun parseCommand(node: Node) {
+        parseSingleCommand(node.child("single-Command"))
         while (currentToken?.kind == Token.Kind.SEMICOLON) {
             acceptIt()
-            parseSingleCommand()
+            parseSingleCommand(node.child("single-Command"))
         }
     }
 
-    private fun parseSingleCommand() {
+    private fun parseSingleCommand(node: Node) {
         when (currentToken?.kind) {
             Token.Kind.IF -> {
                 acceptIt()
-                parseExpression()
+                parseExpression(node.child("Expression"))
                 accept(Token(Token.Kind.THEN, ReservedWords.THEN))
-                parseSingleCommand()
+                parseSingleCommand(node.child("single-Command"))
                 accept(Token(Token.Kind.ELSE, ReservedWords.ELSE))
-                parseSingleCommand()
+                parseSingleCommand(node.child("single-Command"))
             }
             Token.Kind.WHILE -> {
                 acceptIt()
-                parseExpression()
+                parseExpression(node.child("Expression"))
                 accept(Token(Token.Kind.DO, ReservedWords.DO))
-                parseSingleCommand()
+                parseSingleCommand(node.child("single-Command"))
             }
             Token.Kind.LET -> {
                 acceptIt()
-                parseDeclaration()
+                parseDeclaration(node.child("Declaration"))
                 accept(Token(Token.Kind.IN, ReservedWords.IN))
-                parseSingleCommand()
+                parseSingleCommand(node.child("single-Command"))
             }
             Token.Kind.BEGIN -> {
                 acceptIt()
-                parseCommand()
+                parseCommand(node.child("Command"))
                 accept(Token(Token.Kind.END, ReservedWords.END))
             }
             Token.Kind.IDENTIFIER -> {
-                parseIdentifier()
+                parseIdentifier(node.child("Identifier"))
                 when (currentToken?.kind) {
                     Token.Kind.BECOMES -> {
                         acceptIt()
-                        parseExpression()
+                        parseExpression(node.child("Expression"))
                     }
                     Token.Kind.LPAREN -> {
                         acceptIt()
-                        parseExpression()
+                        parseExpression(node.child("Expression"))
                         accept(Token(Token.Kind.RPAREN, Characters.RIGHT_PAREN.toString()))
                     }
                     else -> {
@@ -93,28 +94,28 @@ class Parser(
         }
     }
 
-    private fun parseExpression() {
-        parsePrimaryExpression()
+    private fun parseExpression(node: Node) {
+        parsePrimaryExpression(node.child("primary-Expression"))
         while (currentToken?.kind == Token.Kind.OPERATOR) {
-            parseOperator()
-            parsePrimaryExpression()
+            parseOperator(node.child("Operator"))
+            parsePrimaryExpression(node.child("primary-Expression"))
         }
     }
 
-    private fun parsePrimaryExpression() {
+    private fun parsePrimaryExpression(node: Node) {
         when (currentToken?.kind) {
             Token.Kind.INT_LITERAL -> {
-                parseIntegerLiteral()
+                parseIntegerLiteral(node.child("Int-Literal"))
             }
             Token.Kind.IDENTIFIER -> {
-                parseIdentifier()
+                parseIdentifier(node.child("Identifier"))
             }
             Token.Kind.OPERATOR -> {
-                parseOperator()
+                parseOperator(node.child("Operator"))
             }
             Token.Kind.LPAREN -> {
                 acceptIt()
-                parseExpression()
+                parseExpression(node.child("Expression"))
                 accept(Token(Token.Kind.RPAREN, Characters.RIGHT_PAREN.toString()))
             }
             else -> {
@@ -123,27 +124,27 @@ class Parser(
         }
     }
 
-    private fun parseDeclaration() {
-        parseSingleDeclaration()
+    private fun parseDeclaration(node: Node) {
+        parseSingleDeclaration(node.child("single-Declaration"))
         while (currentToken?.kind == Token.Kind.SEMICOLON) {
             acceptIt()
-            parseSingleDeclaration()
+            parseSingleDeclaration(node.child("single-Declaration"))
         }
     }
 
-    private fun parseSingleDeclaration() {
+    private fun parseSingleDeclaration(node: Node) {
         when (currentToken?.kind) {
             Token.Kind.CONST -> {
                 acceptIt()
-                parseIdentifier()
+                parseIdentifier(node.child("Identifier"))
                 accept(Token(Token.Kind.IS, Characters.TILDE.toString()))
-                parseExpression()
+                parseExpression(node.child("Expression"))
             }
             Token.Kind.VAR -> {
                 acceptIt()
-                parseIdentifier()
+                parseIdentifier(node.child("Identifier"))
                 accept(Token(Token.Kind.COLON, Characters.COLON.toString()))
-                parseIdentifier()
+                parseIdentifier(node.child("Identifier"))
             }
             else -> {
                 throwError()
@@ -151,7 +152,7 @@ class Parser(
         }
     }
 
-    private fun parseOperator() {
+    private fun parseOperator(node: Node) {
         if (currentToken?.kind == Token.Kind.OPERATOR) {
             currentToken = scanner.scan()
         } else {
@@ -159,7 +160,7 @@ class Parser(
         }
     }
 
-    private fun parseIdentifier() {
+    private fun parseIdentifier(node: Node) {
         if (currentToken?.kind == Token.Kind.IDENTIFIER) {
             currentToken = scanner.scan()
         } else {
@@ -167,7 +168,7 @@ class Parser(
         }
     }
 
-    private fun parseIntegerLiteral() {
+    private fun parseIntegerLiteral(node: Node) {
         if (currentToken?.kind == Token.Kind.INT_LITERAL) {
             currentToken = scanner.scan()
         } else {
